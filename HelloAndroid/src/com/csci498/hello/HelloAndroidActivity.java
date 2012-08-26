5ab3b73ab428a03acbe819d6187f9b979552eb77
Joshua Shaw <jshaw@mines.edu>
2012-08-26 15:56:09 -0600

    Rewriting of my first project (for this class) :D

--- HelloAndroid/src/com/csci498/hello/HelloAndroidActivity.java
+++ HelloAndroid/src/com/csci498/hello/HelloAndroidActivity.java
@@ -2,12 +2,26 @@ package com.csci498.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class HelloAndroidActivity extends Activity implements View.OnClickListener {
+  Button btn;
+  @Override
+  	public void onCreate(Bundle icicle) {
+	  super.onCreate(icicle);
+	  btn=new Button(this);
+	  btn.setOnClickListener(this);
+	  updateTime();
+	  setContentView(btn);
+  	}
+  
+  	public void onClick(View view) {
+	  updateTime();
+  	}
+  
+	private void updateTime() {
+	    btn.setText(new Date().toString());
+	  }
 }
