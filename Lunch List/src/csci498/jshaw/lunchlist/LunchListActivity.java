f95bdc7917bb94b52dfd490c18f36d980bc96b1d
Joshua Shaw <jshaw@mines.edu>
2012-09-03 13:45:00 -0600

    Initial commit of restaurant adapter fully implemented

--- Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
+++ Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.*;
 
 public class LunchListActivity extends Activity {
     /** Called when the activity is first created. */
@@ -64,5 +67,50 @@ public class LunchListActivity extends Activity {
     			RestaurantAdapter() {
     		    super(LunchListActivity.this, android.R.layout.simple_list_item_1, model);
     		  }
-    	}
-    }
+    			
+    			public View getView(int position, View convertView, ViewGroup parent) {
+    					View row=convertView;
+    					RestaurantHolder holder = null;
+    					if (row==null) {                         
+    					    LayoutInflater inflater=getLayoutInflater();   					    
+    					    row=inflater.inflate(R.layout.row, parent, false);
+    					    
+    					    holder=new RestaurantHolder(row);
+    					    row.setTag(holder);
+    					  }
+    					else {
+    					    holder=(RestaurantHolder)row.getTag();
+    					  }
+    					  
+    					  holder.populateFrom(model.get(position));
+    					return(row);
+    			}
+    	  }
+    	  
+    	  static class RestaurantHolder {
+    		  private TextView name=null;
+    		  private TextView address=null;
+    		  private ImageView icon=null;
+    		  
+    		  RestaurantHolder(View row) {
+    			  name=(TextView)row.findViewById(R.id.title);
+    			  address=(TextView)row.findViewById(R.id.address);
+    			  icon=(ImageView)row.findViewById(R.id.icon);
+    		  }
+    		  
+    		  void populateFrom(Restaurant r) {
+    			  name.setText(r.getName());
+    			  address.setText(r.getAddress());
+    			  
+    			  if (r.getType().equals("sit_down")) {
+    			      icon.setImageResource(R.drawable.sit_down);
+    			    }
+    			    else if (r.getType().equals("take_out")) {
+    			      icon.setImageResource(R.drawable.take_out);
+    			    }
+    			    else {
+    			      icon.setImageResource(R.drawable.delivery);
+    			    }
+    		  }
+    	  }
+}
