c39e277b21a16486116362060dc068a37ac7e8b2
Joshua Shaw <jshaw@mines.edu>
2012-09-19 17:13:54 -0600

    Added getAll function to retreive data from db, added cursor accesor functions

--- Lunch List/src/csci498/jshaw/lunchlist/RestaurantHelper.java
+++ Lunch List/src/csci498/jshaw/lunchlist/RestaurantHelper.java
@@ -2,6 +2,7 @@ package csci498.jshaw.lunchlist;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -38,4 +39,28 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		getWritableDatabase().insert("restaurants", "name", cv);
 	}
 
+	public Cursor getAll()
+	{
+		return(getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes FROM restaurants ORDER BY name", null));
+	}
+	
+	public String getName(Cursor c) 
+	{
+		return(c.getString(1));
+	}
+	
+	public String getAddress(Cursor c) 
+	{
+		return(c.getString(2));
+	}
+	
+	public String getType(Cursor c) 
+	{
+		return(c.getString(3));
+	}
+	
+	public String getNotes(Cursor c) 
+	{
+		return(c.getString(4));
+	}
 }
