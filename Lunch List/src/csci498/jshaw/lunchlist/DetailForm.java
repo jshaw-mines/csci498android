67004bb20d289a2436e18e13bcba90e98cd9f502
Joshua Shaw <jshaw@mines.edu>
2012-09-25 10:09:19 -0600

    Added detail form activity stub. Added it to the manifest. Added on click logic to display page.

---
+++ Lunch List/src/csci498/jshaw/lunchlist/DetailForm.java
@@ -0,0 +1,12 @@
+package csci498.jshaw.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
--- Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
+++ Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
@@ -9,6 +9,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -139,23 +140,8 @@ public class LunchListActivity extends TabActivity {
     	  private AdapterView.OnItemClickListener onListClick=new AdapterView.OnItemClickListener() {
     		  
     			    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    			    	model.moveToPosition(position);
-    			        
-    			        name.setText(helper.getName(model));
-    			        address.setText(helper.getAddress(model));
-    			        note.setText(helper.getNotes(model));
-    			        
-    			        if (helper.getType(model).equals("sit_down")) {
-    			          types.check(R.id.sit_down);
-    			        }
-    			        else if (helper.getType(model).equals("take_out")) {
-    			          types.check(R.id.take_out);
-    			        }
-    			        else {
-    			          types.check(R.id.delivery);
-    			        }
-    			        
-    			        getTabHost().setCurrentTab(1);
+    			    	Intent i = new Intent(LunchListActivity.this, DetailForm.class);
+    			    	startActivity(i);
     			    }
     	  };
     	  
