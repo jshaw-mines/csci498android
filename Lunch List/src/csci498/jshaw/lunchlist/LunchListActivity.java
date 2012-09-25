b6ad7297d7fa47909386fd17f35f41d1d9a709d4
Joshua Shaw <jshaw@mines.edu>
2012-09-25 10:59:55 -0600

    Added menu and menu logic to LunchList

--- Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
+++ Lunch List/src/csci498/jshaw/lunchlist/LunchListActivity.java
@@ -110,4 +110,23 @@ public class LunchListActivity extends ListActivity {
     		  super.onDestroy();
     		  helper.close();
     	  }
+    	  
+    	  @Override
+    		 public boolean onCreateOptionsMenu(Menu menu)
+    		 {
+    			 new MenuInflater(this).inflate(R.menu.option, menu);
+    			 
+    			 return(super.onCreateOptionsMenu(menu));
+    		 }
+    		 
+    		 @Override
+    		 public boolean onOptionsItemSelected(MenuItem item)
+    		 {
+    			 	if (item.getItemId()==R.id.add) {
+    			 		startActivity(new Intent(LunchListActivity.this, DetailForm.class));
+    			 		return(true);
+    			 	}
+    			 
+    			 	return(super.onOptionsItemSelected(item));
+    		 }
 }
