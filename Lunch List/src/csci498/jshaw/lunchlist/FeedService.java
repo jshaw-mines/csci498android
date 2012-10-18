a1c882593310aa50b9d025e06742b5bd5331336e
Joshua Shaw <jshaw@mines.edu>
2012-10-18 12:30:19 -0600

    Added FeedService stub, added to manifest

---
+++ Lunch List/src/csci498/jshaw/lunchlist/FeedService.java
@@ -0,0 +1,17 @@
+package csci498.jshaw.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+
+	public FeedService(String name) {
+		super(name);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+
+	}
+
+}
